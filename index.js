3b5cffe7d59b9b6c97f1e28fa4df59068a1b6de1
DanielaRF06 <dani_ruizdgo@hotmail.com>
2019-07-03 15:37:44 -0500

    add methods marca on index

--- index.js
+++ index.js
@@ -46,20 +46,49 @@ async function delCliente(idCliente){
      console.log(del);
 }
   /************  MARCAS  */
-/*
-async function crearMarca(){
-    var m = {
-        marca:"Lenovo"
-    };
 
+async function crearMarca(){
+        var m ={
+            marca:"Honda"
+        }
      var marcaCreada = await marcaController.insertarMarca(m,Marca);
      console.log("Marca creada");
      console.log(marcaCreada);
 }
-  */
+ async function allMarcas(){
+    var buscarTodas = await marcaController.allMarcas(Marca);
+     console.log("Todos los registros");
+     console.log(buscarTodas);
+}
+async function unaMarca(m){
+    
+    var buscarUna = await marcaController.oneMarca(m,Marca);
+     console.log(" Registro ");
+     console.log(buscarUna);
+}
+async function upMarca(id,nomM) {
+     var actualizar = await marcaController.updateMarca(id,nomM,Marca);
+     console.log(" Registro ");
+     console.log(actualizar);
+}
+async function delMarca(idMarca){
+    var del = await marcaController.deleteMarca(idMarca,Marca);
+     console.log(" Eliminado ");
+     console.log(del);
+}
+        /***    Clientes */
 //crearCliente();
 //allClientes();
 //oneCl("ABCJ880326A7A");
 //upCliente("5d1ce4fcfed96115b862ccbf","otro","otro","otro");
-delCliente("5d1ce4fcfed96115b862ccbf");
+//  |delCliente("5d1ce4fcfed96115b862ccbf");
+
+        /**    Marcas */
+
+
+//crearMarca();
+//allMarcas();
+//unaMarca("Lenovo");
+//upMarca("5d1ce4fcfed96115b862ccc0","Toshiba 2.0");
+delMarca("5d1d10bde655352a582da09e")
 
